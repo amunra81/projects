786d8c5ca7a9babdf7b1b877851299e71f997588
Bogdan Manole <manoleb>
2013-05-02 10:49:27 +0200

    ⇶

--- haskell/Digester/Html.hs
+++ haskell/Digester/Html.hs
@@ -2,7 +2,8 @@ import Data.List
 import Text.XML.HXT.Core
 
 main = do
-  html <- readFile "Data/test.html"
+  html <- readFile "Data\\test.html"
   let doc = readString [withParseHTML yes, withWarnings no] html
   links <- runX $ doc //> hasName "a" >>> getAttrValue "href"
   mapM_ putStrLn links
+-- ⇶
