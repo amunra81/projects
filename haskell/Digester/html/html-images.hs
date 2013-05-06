aa84f081274a297392230b5b6d76990182ad1256
Bogdan Manole <manoleb>
2013-05-06 17:28:32 +0200

    application patter

--- haskell/Digester/html/html-images.hs
+++ haskell/Digester/html/html-images.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main
     = do
       --argv <- getArgs
-      (al, src, dst) <- cmdlineOpts ["http://www.google.be", "ilidenta.html"] 
+      (al, src, dst) <- cmdlineOpts ["http://haskell.com", "ilidenta.html"] 
       [rc]  <- runX (application al src dst)
       if rc >= c_err
 	  then exitWith (ExitFailure (0-1))
@@ -19,7 +19,6 @@ main
  
 -- | the dummy for the boring stuff of option evaluation,
 -- usually done with 'System.Console.GetOpt'
- 
 cmdlineOpts 	:: [String] -> IO (SysConfigList, String, String)
 cmdlineOpts argv
     = return ([withValidate no, withParseHTML yes,withHTTP []], argv!!0, argv!!1)
@@ -27,17 +26,40 @@ cmdlineOpts argv
 -- | the main arrow
 application	:: SysConfigList -> String -> String -> IOSArrow b Int
 application cfg src dst
-    = configSysVars cfg                                           -- (0)
+    = configSysVars (withTrace 1 : cfg)                           -- (0)
+      >>>
+      traceMsg 1 "start reading document"                         -- (1)
       >>>
       readDocument [] src
       >>>
-      processChildren (processDocumentRootElement `when` isElem)  -- (1)
+      traceMsg 1 "document read, start processing"                -- (2)
+      >>> 
+      processChildren (processDocumentRootElement `when` isElem)
       >>>
-      writeDocument [] dst                                        -- (3)
+      traceMsg 1 "document processed"                             -- (3)
       >>>
-      getErrStatus
- 
+      writeDocument [] dst
+      >>>
+      getErrStatus 
+
 -- | the dummy for the real processing: the identity filter
 processDocumentRootElement	:: IOSArrow XmlTree XmlTree
 processDocumentRootElement
     = this         -- substitute this by the real application
+
+selectAllText	:: ArrowXml a => a XmlTree XmlTree
+selectAllText
+    = deep isText
+
+selectAllTextAndAltValues	:: ArrowXml a => a XmlTree XmlTree
+selectAllTextAndAltValues
+    = deep
+      ( isText                       -- (1)
+        <+>
+        ( isElem >>> hasName "img"   -- (2)
+          >>>
+          getAttrValue "alt"         -- (3)
+          >>>
+          mkText                     -- (4)
+        )
+      )
