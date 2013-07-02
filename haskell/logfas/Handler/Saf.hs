889cb3495aa0257e12729ca5811994e7cf80d218
horus <horus@debian>
2013-07-02 04:02:46 -0400

    playing around

--- haskell/logfas/Handler/Saf.hs
+++ haskell/logfas/Handler/Saf.hs
@@ -3,7 +3,8 @@ module Handler.Saf where
 import Import
 import Yesod.Core.Types
 
-getSafR :: Handler Html
+--getSafR :: Handler Html
+getSafR ::  HandlerT App IO Html
 getSafR = defaultLayout [whamlet| Pula bocanc |] 
 --getSafR = HandlerT $ \ x -> return ([hamlet|asdada|] x)
 
