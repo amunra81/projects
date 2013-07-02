d6a13bac0896d7a3e2c642bf7b316d209c6194a9
horus <horus@debian>
2013-07-02 11:15:45 -0400

    some json

--- haskell/logfas/Handler/Saf.hs
+++ haskell/logfas/Handler/Saf.hs
@@ -3,20 +3,28 @@ module Handler.Saf where
 import Import
 import Yesod.Core.Types
 import Yesod.Core.Json
-
+import Data.List (head)
 
 instance ToJSON Force where
-   toJSON (Force name nation parentId weight) = object [ "name"     .= name  
-                                                       , "nation"   .= nation 
-                                                       , "parent"   .= parentId 
-                                                       , "wight"    .= weight ]
+   toJSON (Force name nation parentId weight) = 
+        object [ "name"     .= name  
+               , "nation"   .= nation 
+               , "parent"   .= parentId 
+               , "wight"    .= weight ]
 
-getSafR :: HandlerT App IO Value 
+getSafR :: HandlerT App IO TypedContent 
 getSafR =  do 
-             fors <- runDB $ do 
+             forces <- runDB $ do 
                         insert $ Force "Force" "Nation" Nothing 2
                         selectList [] [Asc ForceName]
-             returnJson fors
+             let json = return forces
+                 widget = [whamlet|
+                    <ul>
+                       $forall Entity _ (Force name nation _ weight) <- forces
+                        <li> #{name} - #{name} - #{weight}
+                 |]
+
+             defaultLayoutJson widget json
 --getSafR = defaultLayout [whamlet| Pula bocanc |] 
 --getSafR = HandlerT $ \ x -> return ([hamlet|asdada|] x)
 
