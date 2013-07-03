44f6df0af69ec4b3513c305cb3ebdb08401a2d5d
Bogdan Manole <manoleb@gmail.com>
2013-07-03 02:32:00 +0200

    jsoane

--- haskell/logfas/Handler/Saf.hs
+++ haskell/logfas/Handler/Saf.hs
@@ -14,19 +14,43 @@ instance ToJSON Force where
 
 getSafR :: HandlerT App IO TypedContent 
 getSafR =  do 
+             -- fetch data
              forces <- runDB $ do 
                         insert $ Force "Force" "Nation" Nothing 2
                         selectList [] [Asc ForceName]
+             
+             -- presenter
              let json = return forces
-                 widget = [whamlet|
-                    <ul>
-                       $forall Entity _ (Force name nation _ weight) <- forces
-                        <li> #{name} - #{name} - #{weight}
+                 widget = do
+                  addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"
+                  toWidget [julius|
+                    $(function(){
+                        $("#ajax a").click(function(){
+                            $.ajax({
+                                headers: { 
+        Accept : "text/plain; charset=utf-8",
+        "Content-Type": "text/plain; charset=utf-8"
+    }
+                                url : $(this).attr("href"),
+                                success : function(response) {
+                                    $("#res").html(response);
+                                }
+                            })
+                            return false;
+                        });
+                    });
+                    |]
+                  [whamlet|
+                    <ul #ajax>
+                       $forall Entity id (Force name nation _ weight) <- forces
+                            <a href="@{ForceR id}"> #{name} - #{name} - #{weight}
+                    <div #res> Rezultat aici
                  |]
 
              defaultLayoutJson widget json
---getSafR = defaultLayout [whamlet| Pula bocanc |] 
---getSafR = HandlerT $ \ x -> return ([hamlet|asdada|] x)
 
-postSafR :: Handler Html
-postSafR = error "Not yet implemented: post SafR"
+getForceR forceId = do
+            force <- runDB $ get $ forceId
+            let json = return $ toJSON force
+                widget = [whamlet| Is like you would have the javascript off |]
+            defaultLayoutJson widget json
