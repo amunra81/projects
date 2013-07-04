f664da36d891123c85bd9241b0356894b6ccba1f
horus <horus@debian>
2013-07-04 11:28:51 -0400

    some

--- haskell/logfas/Handler/Experiment.hs
+++ haskell/logfas/Handler/Experiment.hs
@@ -1,9 +1,16 @@
 module Handler.Experiment where
 import Import
+import Text.Julius(ToJavascript)
 
 getExperimentR :: Handler Html
 getExperimentR = error ""
 
+data Div = Div { 
+    ident :: Text, 
+    click :: ToJavascript a => a -> WidgetT site m () -- maybe is just to compile easier in the design process 
+    }
 
-clickedDiv :: Monad m => WidgetT App m ()
-clickedDiv = error  ""
+clickedDiv = do
+              id <- newIdent
+              [whamlet|<h1 .#{id}>My Header|]
+              return $ Div id $ error ""
