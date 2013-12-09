cfaf38327fd4668883d9055aa7c4e12f9992d99c
amunra81 <bogdan.manole@excuse.ro>
2013-12-09 18:04:06 +0200

    asda

--- haskell/Digester/WebCollector.hs
+++ haskell/Digester/WebCollector.hs
@@ -1,6 +1,13 @@
-module WebCollector () where
+module WebCollector (
+ExternAction(..),
+NodePos,ParentPos
+) where
+
 type NodePos = Integer
 type ParentPos = Integer
 
 data ExternAction = AddNode ParentPos NodePos |
                     Nothing
+
+
+-- read here : http://johnlato.blogspot.ro/2012/10/runtime-meta-programming-in-haskell.html
