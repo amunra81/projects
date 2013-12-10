0f802ca9aace2522014882cf1774a80157abb74a
Bogdan Manole <manoleb@gmail.com>
2013-12-10 19:13:31 +0200

    ...

--- haskell/Digester/WebCollector.hs
+++ haskell/Digester/WebCollector.hs
@@ -6,8 +6,9 @@ NodePos,ParentPos
 type NodePos = Integer
 type ParentPos = Integer
 
-data ExternAction = AddNode ParentPos NodePos |
-                    Nothing
+data ExternAction  a =  AddRoot a |
+                        AddNode ParentPos NodePos |
+                        Nothing
 
 
 -- read here : http://johnlato.blogspot.ro/2012/10/runtime-meta-programming-in-haskell.html
