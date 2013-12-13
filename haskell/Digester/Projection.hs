c16ecc68e55803d03ab7e3c1d9d717045dc58041
amunra81 <bogdan.manole@excuse.ro>
2013-12-13 11:02:57 +0200

    ...

--- haskell/Digester/Projection.hs
+++ haskell/Digester/Projection.hs
@@ -4,7 +4,8 @@ module Projection (
 -- * projection tree constructors
 proot,pnode,pleaf,pnodeOrLeaf
 -- * projection utils
-,project,projectToRoot,MonadListT(..)
+,MonadListT(..)
+--,project,projectToRoot
 ) where
 
 import Tree
@@ -20,17 +21,17 @@ import Control.Monad.Trans.Identity(IdentityT)
 type Proj a = ProjX IdentityT a
 data ProjX r a = forall m. ( Monad m, MonadListT m, MonadListTX r ) => ProjX ( Div (r m) a )
 
-proot :: (Monad m, MonadListT m) => Div (r m) a -> [PassParent (ProjX r a)] -> Tree (ProjX r a)
-proot n xs = root (ProjX n) xs
+proot :: (Monad m, MonadListT m, MonadListTX r) => Div (r m) a -> [PassParent (ProjX r a)] -> Tree (ProjX r a)
+proot = root . ProjX 
 
-pnode :: (Monad m, MonadListT m) => Div (r m) a -> [PassParent (ProjX r a)] -> Tree (ProjX r a)
-pnode n xs = node (ProjX n) xs
+pnode :: (Monad m, MonadListT m, MonadListTX r) => Div (r m) a -> [PassParent (ProjX r a)] -> PassParent (ProjX r a)
+pnode = node . ProjX 
 
-pleaf :: (Monad m, MonadListT m) => Div m a -> PassParent (ProjX r a)
-pleaf n = leaf (ProjX n) 
+pleaf ::  (Monad m, MonadListT m, MonadListTX r) => Div (r m) a -> PassParent (ProjX r a)
+pleaf = leaf . ProjX 
 
-pnodeOrLeaf :: (Monad m, MonadListT m) => Div (r m) a -> [PassParent (ProjX r a)] -> Tree (ProjX r a)
-pnodeOrLeaf n xs = nodeOrLeaf (ProjX n) xs
+pnodeOrLeaf :: (Monad m, MonadListT m, MonadListTX r) => Div (r m) a -> [PassParent (ProjX r a)] -> PassParent (ProjX r a)
+pnodeOrLeaf = nodeOrLeaf . ProjX 
 
 class MonadListT m where
    toListT :: m a -> ListT IO a
@@ -56,30 +57,31 @@ instance MonadListT (MaybeT IO) where
          Just a -> return [a]
          Nothing -> return []
 
-projectNode :: Proj t -> Tree t -> ListT IO (Tree t)
-projectNode (Proj div) tree = toListT $ runDiv div tree 
-
-project :: Tree (Proj a) -> Tree a -> ListT IO (PassParent a)
-project prjTree tree = do 
-    tnode <- projectNode (value prjTree) tree 
-    let childSeq = case children prjTree of 
-                   None -> toListT []
-                   HasChildren xs -> do -- ListT IO (PassParent a)
-                                    x <- toListT xs
-                                    project x tnode 
-    lift $ do  -- :: IO 
-        xs <- runListT $ childSeq -- :: IO [PassParent a], xs is a list
-        return $ nodeOrLeaf (value tnode) xs -- :: IO (PassParent)
-
-projectToRoot ::  Tree (Proj a) -> Tree a -> ListT IO (Tree a)
-projectToRoot  prjTree tree = do 
-        tnode <- projectNode (value prjTree) tree -- 
-        let passParentList = case children prjTree of 
-                          None -> toListT []
-                          HasChildren xs -> do 
-                                            x <- toListT xs -- ListT IO (PassParent a)
-                                            project x tnode -- :: [PassParent t]
-
-        lift $ do 
-            xs <- runListT $ passParentList -- :: IO [PassParent a], xs is a list
-            return $ root (value tnode) xs
+--projectNode :: ProjX r t -> Tree t -> r (ListT IO) (Tree t)
+--projectNode (ProjX rt) tree = toListTX $ rt 
+----toListTX $ runDiv div tree 
+--
+--project :: Tree (Proj a) -> Tree a -> ListT IO (PassParent a)
+--project prjTree tree = do 
+--    tnode <- projectNode (value prjTree) tree 
+--    let childSeq = case children prjTree of 
+--                   None -> toListT []
+--                   HasChildren xs -> do -- ListT IO (PassParent a)
+--                                    x <- toListT xs
+--                                    project x tnode 
+--    lift $ do  -- :: IO 
+--        xs <- runListT $ childSeq -- :: IO [PassParent a], xs is a list
+--        return $ nodeOrLeaf (value tnode) xs -- :: IO (PassParent)
+--
+--projectToRoot ::  Tree (Proj a) -> Tree a -> ListT IO (Tree a)
+--projectToRoot  prjTree tree = do 
+--        tnode <- projectNode (value prjTree) tree -- 
+--        let passParentList = case children prjTree of 
+--                          None -> toListT []
+--                          HasChildren xs -> do 
+--                                            x <- toListT xs -- ListT IO (PassParent a)
+--                                            project x tnode -- :: [PassParent t]
+--
+--        lift $ do 
+--            xs <- runListT $ passParentList -- :: IO [PassParent a], xs is a list
+--            return $ root (value tnode) xs
