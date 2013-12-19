9428a84134a9b498aa40889236f0c31a27a0040f
amunra81 <bogdan.manole@excuse.ro>
2013-12-19 17:58:54 +0200

    ...

--- haskell/Digester/TypeFamiliesPlay.hs
+++ haskell/Digester/TypeFamiliesPlay.hs
@@ -1,3 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
+module TypeFamiliesPlay where
 
+class MonadList m where
+  data MList :: * -> *   
+  toList :: m a -> MList a
 
+class MonadListT n where
+  toListM :: (MonadList m) => n m a -> n MList a 
