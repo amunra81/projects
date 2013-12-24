92174f238ff14513917894462881eb4bf78688d4
amunra81 <bogdan.manole@excuse.ro>
2013-12-24 13:10:27 +0200

    C-F2

--- haskell/Digester/TreeM.hs
+++ haskell/Digester/TreeM.hs
@@ -5,8 +5,8 @@
 {-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module TreeM () where 
-import Control.Monad.Trans.Class(MonadTrans)
+module TreeM (
+Tree,na,node,root,leaf) where 
 
 na ::  t
 na = error ""
@@ -22,31 +22,7 @@ data Tree m a = Root a (m (Tree m a)) |                 -- value , children
                 Node a (m (Tree m a)) (Tree m a) Pos |  -- value , children , parent , position
                 Leaf a (Tree m a) Pos                   -- value , parent , position
 
--- |constructor for a node of type root. first argurment is the value, and the second the list of passparent elements.
--- the returning part is the tree node
---root ::  a -> m (PassParent m a) -> Tree m a
---root a ms = 
---           Root a zs
---              where
---              zs = map toNode $ zip [0..] xs
---              toNode (pos,passParent) = passParent p pos 
---              p = root a xs
-
--- |constructor for a node of type leaf. the single argurment is the value, and the returning element is a passparent
-leaf :: a -> PassParent m a
-leaf a p i = Leaf a p i
-
--- |constructor for a node of type node. first argurment is the value, and the second the list of passparent elements
--- |the returning part is a passparent as wel
-
-node :: (Monad m,Countable m) => a -> m (PassParent m a) -> PassParent m a
-node a ms p pos =
-           Node a cs p pos
-              where 
-              cs = do 
-                    (passParent,pos) <- count ms 
-                    return $ passParent p pos 
-
+-- * Countable class
 class Countable m where
   count :: m a -> m (a,Int)
 
@@ -56,3 +32,33 @@ instance Countable [] where
 instance Countable Maybe where
   count (Just a) = Just (a,0)              
   count Nothing = Nothing
+
+-- |constructor for a node of type leaf. the single argurment is the value, and the returning element is a passparent
+leaf :: a -> PassParent m a
+leaf a p i = Leaf a p i
+
+-- |constructor for a node of type node. first argurment is the value, and the second the list of passparent elements
+-- |the returning part is a passparent as wel
+node :: (Monad m,Countable m) => a -> m (PassParent m a) -> PassParent m a
+node a ms p pos =
+           Node a cs p pos
+              where 
+              cs = do 
+                    (passParent,pos) <- count ms 
+                    return $ passParent p pos 
+
+-- |constructor for a node of type root. first argurment is the value, and the second the list of passparent elements.
+-- the returning part is the tree node
+root :: (Monad m,Countable m) => a -> m (PassParent m a) -> Tree m a
+root a xs = 
+           Root a cs
+              where
+              cs = do
+                    (passParent,pos) <- count xs
+                    return $ passParent p pos
+              p = root a xs
+
+node :: (Monad m,Countable m) => a -> m (PassParent m a) -> PassParent m a
+nodeOrLeaf a (x:xs) = node a (x:xs)
+nodeOrLeaf a [] = leaf a
+
