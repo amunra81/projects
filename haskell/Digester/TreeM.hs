54c92b4254dda28effd6fd84ac03f0593d3cd7b5
amunra81 <bogdan.manole@excuse.ro>
2013-12-24 14:02:28 +0200

    C-F2

--- haskell/Digester/TreeM.hs
+++ haskell/Digester/TreeM.hs
@@ -5,8 +5,8 @@
 {-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module TreeM (
-Tree,na,node,root,leaf) where 
+module TreeM (Tree,na,node,root,leaf,toPassParent,value,position,index,commonIndexes) where 
+import Data.Monoid
 
 na ::  t
 na = error ""
@@ -44,8 +44,9 @@ node a ms p pos =
            Node a cs p pos
               where 
               cs = do 
-                    (passParent,pos) <- count ms 
-                    return $ passParent p pos 
+                    (passParent,cpos) <- count ms 
+
+                    return $ passParent p cpos 
 
 -- |constructor for a node of type root. first argurment is the value, and the second the list of passparent elements.
 -- the returning part is the tree node
@@ -58,7 +59,52 @@ root a xs =
                     return $ passParent p pos
               p = root a xs
 
-node :: (Monad m,Countable m) => a -> m (PassParent m a) -> PassParent m a
-nodeOrLeaf a (x:xs) = node a (x:xs)
-nodeOrLeaf a [] = leaf a
+toPassParent :: Tree m a -> PassParent m a
+toPassParent tree = case tree of
+                    Root a m -> \ p i -> Node a m p i
+                    Node a m _ _ -> \ p i -> Node a m p i 
+                    Leaf a _ _ -> \ p i -> Leaf a p i
+
+-- |fetch the value from a node
+value ::  Tree m a -> a
+value (Root v _)     = v 
+value (Node v _ _ _) = v
+value (Leaf v _ _ )  = v
+
+-- |fetch the node position
+position ::  Tree m a -> Pos
+position (Root _ _)      = 0
+position (Node _ _ _ i)  = i
+position (Leaf _ _ i )   = i
+--
+-- |create a list of pos representing the indexes of each node (parents) until the root is reached
+index :: Tree m a -> [Pos]
+index tree = 
+    case tree of
+    Node _ _ p _ -> (position tree : index p) 
+    Leaf _ p _   -> (position tree : index p)
+    Root _ _ -> [0]
+
+-- |take two nodes and create a tubple of two array of indexes. The difference between the indexes created by the "index" function, the indexes cretead by the commonIndexes represent the index of all parents for each node, till a common parent is found. For sure if no other common parent diffrent then root is found, there will be no diference between the indexes created by the "index" function and the "commonIndexes" method
+commonIndexes :: Tree m a -> Tree m a -> ([Pos],[Pos])
+commonIndexes n1 n2= 
+    let reduce i1 i2 = 
+            case (i1,i2) of
+            (x:xs,y:ys) -> if x == y then reduce xs ys 
+                                   else (i1,i2)
+            _ -> (i1,i2)
+    in
+    reduce (index n1) (index n2) 
+--
+-- |instead a list of position, this method return like "index" method the same values, but in a string representation (list of chars). This is good for comparations of nodes.
+stringIndex ::  Tree m a -> [Char]
+stringIndex tree = foldl (\a x -> a ++ (show x)) "" $ index tree
+
+-- |get the children o a node , under the repsentation of a sequence
+children :: Monoid (m (Tree m a)) => Tree m a -> m (Tree m a)
+children tree = case tree of
+                    Node _ xs _ _   -> xs
+                    Root _ xs       -> xs
+                    Leaf _ _ _      -> mempty
+
 
