5ec07f08a95cb8c56ae3ab12bd32d465b8cf3202
amunra81 <bogdan.manole@excuse.ro>
2014-01-30 17:33:33 +0200

    C-F2

--- haskell/Digester/generic/Projection.hs
+++ haskell/Digester/generic/Projection.hs
@@ -12,6 +12,7 @@ import Monad
 import Prelude hiding (div)
 import Control.Monad.List
 
+-- |project a tree based on a tree of divs. The projection result is a M(PassParent)
 project :: (MonadPlus m, Countable m) => Tree m (Div m a) -> Tree m a -> m (PassParent m a)
 project pTree tree = do
         -- project the main node
@@ -19,12 +20,13 @@ project pTree tree = do
 
         -- get the projected children
         let tchildren = do
-                           x <- getChildren pTree
-                           project x tnode
+                            x <- getChildren pTree
+                            project x tnode
 
         -- construct the final node
         return $ node (value tnode) tchildren
 
+-- |project a tree based on a tree of divs. The projection result is a M(Tree)
 projectToRoot :: (MonadPlus m, Countable m) => Tree m (Div m a) -> Tree m a -> m (Tree m a)
 projectToRoot pTree tree = do
         -- get the node from the projection
@@ -38,5 +40,8 @@ projectToRoot pTree tree = do
         -- construct the root
         return $ root (value tnode) tchildren
 
-transform :: Tree m a -> Tree n a
-transform = na
+transform :: (m a -> n a) -> Tree m a -> Tree n a
+transform f n2 = case n2 of
+                  Root a ch -> na
+                  Node a ch parent pos -> na 
+
