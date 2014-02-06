dd34b87e762a2dd238f3149f81eb51c7f372091d
amunra81 <bogdan.manole@excuse.ro>
2014-02-06 19:11:35 +0200

    C-F2

--- haskell/Digester/generic/Web.hs
+++ haskell/Digester/generic/Web.hs
@@ -7,9 +7,9 @@ import Text.XML.HXT.Core hiding (Tree,root,getChildren)
 import Control.Monad.List
 import Control.Monad.Trans.Class(lift)
 
---staticWeb =  do 
---          xs <- runListT domains
---          return $ root (xText "za veb") xs
+staticWeb =  do 
+          xs <- runListT domains
+          return $ root (xText "za veb") xs
 
 xText ::  String -> Html
 xText = Html . XText 
@@ -18,7 +18,7 @@ liftList :: (Monad m) =>  [a] -> ListT m a
 liftList = ListT . return
         
 -- | create the domain nodes
-domains ::  ListT IO (PassParent (ListT IO) Html)
+--domains ::  ListT IO (PassParent (ListT IO) Html)
 domains = do
             a <- liftList ["http://www.google.com"] 
             t <- lift $ downloadTree a
