7425de4e349ab97c117d0c28a66a471dcb08966c
amunra81 <bogdan.manole@excuse.ro>
2014-02-14 17:02:32 +0200

    C-F2

--- haskell/Digester/generic/Projection.hs
+++ haskell/Digester/generic/Projection.hs
@@ -6,11 +6,11 @@
 module Projection (
 -- * projection utils
 project,transform,projectToRoot,projectC,projectToRootC
-) where
+,link,all) where
 
 import Tree
 import Monad
-import Prelude hiding (div)
+import Prelude hiding (div,all)
 import Control.Monad.List
 
 -- |project a tree based on a tree of divs. The projection result is a M(PassParent)
@@ -32,7 +32,7 @@ projectC :: (MonadPlus m, Monad n,Countable m,Convertible n m)
 projectC pTree tree = project (transform pTree) tree
 
 -- |project a tree based on a tree of divs. The projection result is a M(Tree)
-projectToRoot :: ( MonadPlus m, Countable m ) => Tree m (Div m a) -> Tree m a -> m (Tree m a)
+projectToRoot :: (MonadPlus m, Countable m) => Tree m (Div m a) -> Tree m a -> m (Tree m a)
 projectToRoot pTree tree = do
         -- get the node from the projection
         tnode <- runDiv (value pTree) tree
@@ -49,5 +49,8 @@ projectToRootC :: (MonadPlus m, Monad n, Countable m,Convertible n m)
                => Tree n (Div m a) -> Tree m a -> m (Tree m a)
 projectToRootC pTree tree= projectToRoot (transform pTree) tree
 
-all :: Div m a -> m (PassParent m a) 
+all :: Div m a -> m (PassParent m (Div m a)) 
 all = na
+
+link :: Tree m (Div m a) -> Tree m a -> m (PassParent m a)
+link = na 
