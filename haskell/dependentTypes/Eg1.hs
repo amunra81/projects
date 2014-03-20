74a55e2c732e1a25c4562e4616608726c51807e7
amunra81 <bogdan.manole@excuse.ro>
2014-03-20 16:04:24 +0200

    C-F2

---
+++ haskell/dependentTypes/Eg1.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE DataKinds, TypeFamilies #-}
+module Eg1 where
+
+data Nat = Z | S Nat
+
+type family Plus (n :: Nat) (m :: Nat) :: Nat
+
+type instance Plus Z m = m
+type instance Plus (S n) m = S (Plus n m)
