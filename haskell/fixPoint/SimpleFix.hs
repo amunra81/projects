6a004e6f04832f4870842053ad59883b3987aefd
amunra81 <bogdan.manole@excuse.ro>
2014-03-21 14:02:59 +0200

    C-F2

---
+++ haskell/fixPoint/SimpleFix.hs
@@ -0,0 +1,15 @@
+module SimpleFix where
+
+fix' f = f ( fix' f )
+
+x = fix' (1:)
+
+--this is equaivalent to x
+x' = 1:x'
+
+tk = take 3000
+
+y  = tk x
+y' = tk x'
+
+
---
+++ haskell/typefamilies/XList.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE TypeFamilies #-}
+
+-- Declare a list-like data family
+data family XList a
+ 
+-- Declare a list-like instance for Char
+data instance XList Char = XCons !Char !(XList Char) | XNil
+ 
+-- Declare a number-like instance for ()
+data instance XList () = XListUnit !Int
+
+
