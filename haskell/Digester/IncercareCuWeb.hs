be34b53986eb53f4871b72052b9a266c644ada9e
Bogdan Manole <manoleb@gmail.com>
2014-06-08 18:16:06 +0200

    C-F2

--- haskell/Digester/IncercareCuWeb.hs
+++ haskell/Digester/IncercareCuWeb.hs
@@ -1,6 +1,9 @@
 module IncercareCuWeb where 
 
 
-data Page = Page
+data Page           = Page
+data Dom            = Dom
+data Event          = Event
+data Computation    = Comp Event 
 
-data Event = Event
+-- algoritmul este un tree de evenimente copii de evenimente viitoare
