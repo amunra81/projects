04e2a1501b2a4563bed5de41d72838d00df72b6b
Bogdan Manole <manoleb@gmail.com>
2014-08-10 01:16:50 +0300

    C-F2

--- haskell/cuda/MetaProgramming.hs
+++ haskell/cuda/MetaProgramming.hs
@@ -2,7 +2,6 @@ module MetaProgramming where
 
 import Control.Monad.Par.Meta.AccSMP
 import Data.Array.Accelerate
-import Data.Array.Accelerate.CUDA as CUDA
 
 trivEx :: Int -> Acc (Scalar Float)
 trivEx no = let arr = generate (constant (Z :. no)) 
@@ -10,22 +9,29 @@ trivEx no = let arr = generate (constant (Z :. no))
        in fold (+) 0 arr
 
 triv ::  Acc (Scalar Float)
-triv = trivEx 10
+triv = trivEx 4
 
-runDirect :: Int ->  IO ()
-runDirect = print . CUDA.run . trivEx
+h :: Int 
+h = undefined
 
-runWPar1,runWPar2 ::  IO ()
+k = case h of
+    3 -> ""
+--    undefined -> "asdas"
+
+--runDirect :: Int ->  IO ()
+--runDirect = print . CUDA.run . trivEx
+
+runWPar1 :: IO() --,runWPar2 ::  IO ()
 runWPar1 = print $ runPar $ runAcc triv
-runWPar2 = print $ runPar $ runAccWith CUDA.run triv
-
-main :: IO ()
-main = runDirect 10
-
--- unsafe-hibrid
-
-hybrid :: Par (IVar Float)
-hybrid = unsafeHybridWith CUDA.run (`indexArray` Z) 
-                          (return 33.0, triv)
-
-runHybrid = print (runPar (hybrid >>= get))
+--runWPar2 = print $ runPar $ runAccWith CUDA.run triv
+--
+--main :: IO ()
+--main = runDirect 10
+--
+---- unsafe-hibrid
+--
+--hybrid :: Par (IVar Float)
+--hybrid = unsafeHybridWith CUDA.run (`indexArray` Z) 
+--                          (return 33.0, triv)
+--
+--runHybrid = print (runPar (hybrid >>= get))
---
+++ haskell/functors/Exercices.hs
@@ -0,0 +1,30 @@
+module Exercices where
+import Prelude hiding (Either(..))
+
+---------------- Create functor for Either -----------------------
+
+data Either a b = Left a | Right b
+
+instance Functor (Either e) where
+ fmap _ (Left e) = Left e
+ fmap g (Right a) = Right (g a)
+
+instance (Show a, Show b) => Show (Either a b) where
+ show (Left a) = "Left " ++ show a
+ show (Right b) = "Right " ++ show b
+
+testEither :: (Show a,Show e) => Either e a -> Either e String
+testEither = fmap show 
+
+------------------------------------------------------------------
+
+---------------- Create functor for ITree ------------------------
+
+data ITree a = Leaf (Int -> a) |
+               Node [ITree a]
+
+instance Functor ITree where
+ fmap g (Leaf h) = Leaf $ g . h
+ fmap g (Node xs) = Node $ map (fmap g) xs
+
+-----------------------------------------------------------------
