dfa8e0a5aa6069bac5d0075c70f2b53e66f76f8b
Bogdan Manole <manoleb@gmail.com>
2014-09-20 01:25:17 +0200

    C-F2

--- haskell/cuda/MetaProgramming.hs
+++ haskell/cuda/MetaProgramming.hs
@@ -2,6 +2,7 @@ module MetaProgramming where
 
 import Control.Monad.Par.Meta.AccSMP
 import Data.Array.Accelerate
+import Data.Array.Accelerate.CUDA as CUDA
 
 trivEx :: Int -> Acc (Scalar Float)
 trivEx no = let arr = generate (constant (Z :. no)) 
@@ -9,26 +10,20 @@ trivEx no = let arr = generate (constant (Z :. no))
        in fold (+) 0 arr
 
 triv ::  Acc (Scalar Float)
-<<<<<<< HEAD
 triv = trivEx 5002
 
 runDirect :: Int -> IO ()
 runDirect = print . CUDA.run . trivEx
-=======
-triv = trivEx 4
 
-h :: Int 
-h = undefined
->>>>>>> f2a1a4eaaf16e27db5deeb7de006fcdbcb08e8b8
+runWPar1,runWPar2 ::  IO ()
+runWPar1 = print $ runPar $ runAcc triv
+runWPar2 = print $ runPar $ runAccWith CUDA.run triv
 
-k = case h of
-    3 -> ""
---    undefined -> "asdas"
+main :: IO ()
+main = runDirect 10
 
---runDirect :: Int ->  IO ()
---runDirect = print . CUDA.run . trivEx
+-- unsafe-hibrid
 
-<<<<<<< HEAD
 hybrid :: Par (IVar Float)
 hybrid = unsafeHybridWith CUDA.run (`indexArray` Z) 
                           (return 33.0, triv)
@@ -36,19 +31,3 @@ hybrid = unsafeHybridWith CUDA.run (`indexArray` Z)
 runHybrid ::  IO ()
 runHybrid = print (runPar (hybrid >>= get))
 
-=======
-runWPar1 :: IO() --,runWPar2 ::  IO ()
-runWPar1 = print $ runPar $ runAcc triv
---runWPar2 = print $ runPar $ runAccWith CUDA.run triv
---
---main :: IO ()
---main = runDirect 10
---
----- unsafe-hibrid
---
---hybrid :: Par (IVar Float)
---hybrid = unsafeHybridWith CUDA.run (`indexArray` Z) 
---                          (return 33.0, triv)
---
---runHybrid = print (runPar (hybrid >>= get))
->>>>>>> f2a1a4eaaf16e27db5deeb7de006fcdbcb08e8b8
