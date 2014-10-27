010cf6103e29ee46fb3a473ffe2161b522168f18
Manole Bogdan <manoleb@gmail.com>
2014-10-27 14:42:33 -0400

    asdad

--- haskell/ffi/simpleEgOfImport/CallBacker_stub.h
+++ haskell/ffi/simpleEgOfImport/CallBacker_stub.h
@@ -2,7 +2,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern HsDouble Main_d2B7(StgStablePtr the_stableptr, HsDouble a1);
+extern HsDouble Main_d2Bm(StgStablePtr the_stableptr, HsDouble a1);
 #ifdef __cplusplus
 }
 #endif
