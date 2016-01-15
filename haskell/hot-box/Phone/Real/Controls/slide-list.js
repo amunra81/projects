bcc8020bd5ed26a3075886d85de9c8de0b6a3ac0
Bogdan Manole <manoleb@gmail.com>
2016-01-15 10:41:53 +0200

    C-F2

--- haskell/hot-box/Phone/Real/Controls/slide-list.js
+++ haskell/hot-box/Phone/Real/Controls/slide-list.js
@@ -22,7 +22,7 @@ module.exports = React.createClass({
   getInitialState: function() {
       return { 
                 currentPage: 0,
-                containerHeight: 0,
+                containerDims: null,
              };
   },
   componentWillMount: function() {
@@ -38,46 +38,56 @@ module.exports = React.createClass({
 
   componentDidMount: function() {
       console.log("did");
-      setTimeout(this.measureContainer);
+       if(!this.state.containerDims) 
+        setTimeout(this.measureContainer);
   },
 
   measureContainer: function() {
       this.container.measure((fx, fy, width, height, px, py) => {
           var str =  `measured: (fx:${fx} fy:${fy} width:${width} height:${height} px:${px} py:${py})`;
           console.log(str);
+          this.setState({containerDims:{width:width,height:height}});
       });
   },
 
   render: function() {
-    return this.state.containerHeight?this.renderPages():this.renderMeasuring();
+    return this.state.containerDims?this.renderPages():this.renderMeasuring();
   },
 
   renderMeasuring: function() {
-      console.log("measuring...");
-      return (
-          <View ref={x=>this.container = x} style={styles.container} >
+    console.log("measuring...");
+    return (
+        <View ref={x=>this.container = x} style={styles.container} >
             <Text style={{top:-10}}> Measuring ... </Text>       
             <Text> Measuring ... </Text>       
-            <TouchableHighlight  onPress={() => { this.measureContainer() }}>
-                <Text>Measure!</Text>
-            </TouchableHighlight>
         </View>
     );
   },
 
   renderPages: function() {
     //{this.props.dataSource.map( x => this.renderItem(x))} 
+    //<View><Text >{this.props.pageSize} + {this.state.containerDims.height}</Text></View>
     return (<View ref={x=>this.container = x} style={styles.container} >
-                <View><Text >{this.props.pageSize} + {this.state.containerHeight}</Text></View>
                 {this.renderPage(0,{name:"pula",style:{backgroundColor:'red'}})}
                 {this.renderPage(1,{style:{backgroundColor:'green'},...this._panResponder.panHandlers})}
-                {this.renderPage(2,{style:{backgroundColor:'yellow',  width: null, height: null}})}
+                {this.renderPage(2,{style:{backgroundColor:'yellow'}})}
             </View>
     );
   },
 
+  _withCommonStyle: function(propsP) {
+    var {style,props} = propsP;
+    var commonStyle = {
+            top:-this.state.containerDims.height,
+            height:this.state.containerDims.height,
+            width:this.state.containerDims.width,
+    }
+
+    return {style: [commonStyle,style],...props};
+  },
   //Enumerable.From(['a','b','c','d','e','f']).Zip(Enumerable.Range(0,10),"a,b=>a+':'+b")
-  renderPage: function(pageNo,props) {
+  renderPage: function(pageNo,propsP) {
+    var props = this._withCommonStyle(propsP);
     var extraProps = {pageNo:pageNo,...props};
     var text = `Page: ${pageNo}`;
     var {pageSize,dataSource} = this.props;
@@ -133,7 +143,7 @@ var styles = StyleSheet.create({
         flex:1,
         alignItems:'center',
         backgroundColor: '#dcf4ff',
-        marginTop: 120,
+        marginTop: 20,
         overflow:'hidden',
     }
 });
